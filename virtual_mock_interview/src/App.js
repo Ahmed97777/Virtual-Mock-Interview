2551e0233046440bd7de720f5273fe561c2cf3cf
AhmedMohammedKhorkhash <khorkhash3@gmail.com>
2023-03-07 02:01:26 +0200

    Adding React, creating basic structure+welcomeText

---
+++ virtual_mock_interview/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      
+      <h1 className="first" >Welcome to the first commit.</h1>
+
+
+    </div>
+  );
+}
+
+export default App;
