d444b646ecbea34e2c0e93f2cfe7266ed43187f3
AhmedMohammedKhorkhash <khorkhash3@gmail.com>
2023-03-07 14:15:22 +0200

    Formating style and structure.

--- virtual_mock_interview/src/App.js
+++ virtual_mock_interview/src/App.js
@@ -1,12 +1,14 @@
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      
-      <h1 className="first" >Welcome to the first commit.</h1>
+      <h1 className="first">Welcome to the first commit.</h1>
 
+      <div>This is div 1</div>
+      <div>This is div 1</div>
 
+      <p id="pe">This is p 1</p>
     </div>
   );
 }
