d746290fc2a17a0090813823f8f8d70a837dbbd8
AhmedMohammedKhorkhash <khorkhash3@gmail.com>
2023-03-08 14:49:49 +0200

    Adding Landing component HTML, CSS.

--- virtual_mock_interview/src/Components/Landing.js
+++ virtual_mock_interview/src/Components/Landing.js
@@ -4,8 +4,12 @@ const Landing = () => {
     return (
 
         <div className="landing" >
-            
-            
+            <div className="intro-text">
+                <h1 className="websit-title" >Virtual Mock Interview</h1>
+                <p>Empowering job seekers with an intelligent and unbiased interview preparation platforms that leverages advanced technology 
+                    and user-driven insights to increase their chances of landing their dream job.
+                </p>
+            </div>
         </div>
 
     )
