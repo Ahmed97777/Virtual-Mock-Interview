dca6328a14cc0aa5bbe47448a258675dc192f9a6
AhmedMohammedKhorkhash <khorkhash3@gmail.com>
2023-04-10 21:17:06 +0200

    updated field.js to handle buttons ids and confirm

--- virtual_mock_interview/src/Components/Field.js
+++ virtual_mock_interview/src/Components/Field.js
@@ -1,4 +1,5 @@
 import React, { useEffect } from 'react';
+import { useState } from 'react';
 import { Link } from "react-router-dom"
 
 const Field = () => {
@@ -7,27 +8,26 @@ const Field = () => {
         window.scrollTo(0, 0);
         }, []);
 
+        const [choosenField, setChoosenField] = useState('');
 
-
-        const handleButtonClick = (e) => {
-            console.log(e.target.id);
+        const handleButtonClick = (event) => {
+            setChoosenField(event.target.id);
         };
 
-
-
+        const printChoosenField = () => {
+            console.log(choosenField);
+        };
 
 
     return (
 
         <>
 
-
             <div className="logo-container-for-config" >
                 <div className="top-logo-menu-for-config" >
                     <a className="logo-name-for-config" href="/" >VMI</a>
                 </div>
             </div>
-
 
             <div className="instructions-parent" >
             
@@ -36,50 +36,41 @@ const Field = () => {
                     <h2 className="main-title-2" >2- Please choose field:</h2>
                 </div>
 
-
             </div>
 
-
-
             {/* Starting the configuration process */}
-
 
             <div className="config-container">
                 
                 <div className="field-container" >
                     
 
-
-                        <button id='soft-en' onClick={handleButtonClick}>Software Engineering</button>
-                        <button id='elec-en' onClick={handleButtonClick}>Electrical Engineering</button>
-                        <button id='mecha-en' onClick={handleButtonClick}>Mechanical Engineering</button>
-                        <button id='auto-en' onClick={handleButtonClick}>Automation Engineering</button>
-                        <button id='code-g' onClick={handleButtonClick}>Coding General</button>
-                        <button id='jun-web' onClick={handleButtonClick}>Junior Web Development</button>
-                        <button id='sen-web' onClick={handleButtonClick}>Senior Web Development</button>
-                        <button id='frs-web' onClick={handleButtonClick}>Fresh Web Development</button>
-                        <button id='frs-mob' onClick={handleButtonClick}>Fresh Mobile Development</button>
-                        <button id='sen-mob' onClick={handleButtonClick}>Senior Mobile Development</button>
-                        <button id='jun-mob' onClick={handleButtonClick}>Junior Mobile Development</button>
-                        <button id='game-dev' onClick={handleButtonClick}>Game Development</button>
-                        
+                    <button id='soft-en' onClick={handleButtonClick} className={choosenField === 'soft-en' ? 'activeField' : ''}>Software Engineering</button>
+                    <button id='elec-en' onClick={handleButtonClick} className={choosenField === 'elec-en' ? 'activeField' : ''}>Electrical Engineering</button>
+                    <button id='mecha-en' onClick={handleButtonClick} className={choosenField === 'mecha-en' ? 'activeField' : ''}>Mechanical Engineering</button>
+                    <button id='auto-en' onClick={handleButtonClick} className={choosenField === 'auto-en' ? 'activeField' : ''}>Automation Engineering</button>
+                    <button id='code-g' onClick={handleButtonClick} className={choosenField === 'code-g' ? 'activeField' : ''}>Coding General</button>
+                    <button id='jun-web' onClick={handleButtonClick} className={choosenField === 'jun-web' ? 'activeField' : ''}>Junior Web Development</button>
+                    <button id='sen-web' onClick={handleButtonClick} className={choosenField === 'sen-web' ? 'activeField' : ''}>Senior Web Development</button>
+                    <button id='frs-web' onClick={handleButtonClick} className={choosenField === 'frs-web' ? 'activeField' : ''}>Fresh Web Development</button>
+                    <button id='frs-mob' onClick={handleButtonClick} className={choosenField === 'frs-mob' ? 'activeField' : ''}>Fresh Mobile Development</button>
+                    <button id='sen-mob' onClick={handleButtonClick} className={choosenField === 'sen-mob' ? 'activeField' : ''}>Senior Mobile Development</button>
+                    <button id='jun-mob' onClick={handleButtonClick} className={choosenField === 'jun-mob' ? 'activeField' : ''}>Junior Mobile Development</button>
+                    <button id='game-dev' onClick={handleButtonClick} className={choosenField === 'game-dev' ? 'activeField' : ''}>Game Development</button>
+                    
                     
                 </div>
 
-                <Link to="/example-case" ><button className="button-start" >Next <span className="triangle"></span></button></Link>
+                <Link to="/example-case" ><button className="button-start" onClick={printChoosenField}>Next <span className="triangle"></span></button></Link>
             </div>
-
-
 
             {/* Ending the configuration process */}
 
-    
-    
+
         </>
 
     )
 
 }
-
 
 export default Field
